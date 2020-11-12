3396dcf5473a9e19bd4cab4ad3cb24342ef9d079
Thembelihlethemba <thembelihlethemba985@gmail.com>
2020-11-12 10:55:42 +0200

    Add project files.

---
+++ ReadMe.txt
@@ -0,0 +1,12 @@
+#chuck/swapi
+This project is for abstracting two APIs(Chuck Norris API and Star Wars API).It desplay a list of joke categories,
+all star wars people, and create a search semultenously for both Chuck Norris and Star Wars API.
+This is a backend Engineer Test.
+#Technoogies
+Bootstrap 3.3.7
+c#
+swaggar
+#launch
+No minimum requirements
+The 2nd "GET" button require "id"(any integer greater than 0)
+"POST" button require "query"(any string is allowed)
---
+++ WebApplication5/Controllers/ValuesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+using System.IO;
+using System.Dynamic;
+
+namespace WebApplication5.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET /chuck
+        /// <summary>
+        /// chuck
+        /// </summary>
+        /// <returns>list of joke categories</returns>
+        public string Get()
+        {
+            WebClient client = new WebClient();
+            string wbc = client.DownloadString("https://api.chucknorris.io/jokes/categories");
+            return wbc;
+
+        }
+
+        //GET /swapi
+        ///<summary>
+        /// swapi
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>all Star Wars People</returns>
+        public string Get(int id)
+        {
+            dynamic dobj;
+            string wbc1="";
+            if (id > 0)
+            {
+                
+                WebClient client = new WebClient();
+                wbc1 = client.DownloadString("https://swapi.dev/api/people/");
+                dobj = JsonConvert.DeserializeObject<dynamic>(wbc1);
+            }
+            
+            return wbc1;
+        }
+
+        // POST api/values
+        public string Post(SearchOption SearchAPI,string quiry)
+        {
+            SearchAPI=SearchOption.AllDirectories;
+            string wbc2 = "";
+            string wbc3 = "";
+            WebClient client = new WebClient();
+            
+            wbc2 = client.DownloadString("https://api.chucknorris.io/jokes/search?query={query}");
+            wbc3 = client.DownloadString("https://swapi.dev/api/people/?search={query}");
+            quiry = wbc2 + wbc3;
+            return quiry;
+
+        }
+
+    }
+}
